4e3a01f517ec745fa4f87eda5bed5978b1aa087b
JongMany <blackberry1114@naver.com>
2024-06-17 16:16:04 +0900

    style: 메인 화면 UI 변경

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   function getHello() {
     const $greet = document.querySelector("#greet")!;
     fetch("http://localhost:4000/api/hello")
@@ -19,30 +15,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          api called: <code id="greet"></code>
-        </p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          API 호출 결과: <code id="greet"></code>
         </p>
       </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }

aeb2ffb2edec2dd4e0ef4b4f1fcd5a34d1e9bda3
JongMany <blackberry1114@naver.com>
2024-06-19 14:57:42 +0900

    infra: puppeteer 설정 추가

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,15 +1,31 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import prerender from "@prerenderer/rollup-plugin";
+import puppeteerRenderer from "@prerenderer/renderer-puppeteer";
+import puppeteer from "puppeteer";
 
+//https://velog.io/@parallelkim/%EB%A6%AC%EC%95%A1%ED%8A%B8-SEO%EC%97%90-%EA%B4%80%ED%95%9C-%EB%AA%A8%EB%93%A0-%EA%B2%83
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: true, // 브라우저가 백그라운드에서 실행되도록 설정
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto("http://example.com");
+    const html = await page.content();
+    console.log(html);
+    await browser.close();
+  })();
+
   return {
     plugins: [
       react(),
       prerender({
         routes: ["/", "/main"],
-        renderer: "@prerenderer/renderer-puppeteer",
+        renderer: puppeteerRenderer,
         // server: {
         //   port: Number(env.VITE_SERVER_PORT),
         //   host: env.VITE_SERVER_HOST,

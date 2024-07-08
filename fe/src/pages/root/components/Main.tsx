9bf5ca71917a499ed0d29f38a532a5b6ae0194d1
JongMany <blackberry1114@naver.com>
2024-07-08 11:41:47 +0900

    clean: 코드 정리

--- fe/src/pages/root/components/Main.tsx
+++ fe/src/pages/root/components/Main.tsx
@@ -1,18 +1,8 @@
 import Contents from "@/pages/root/components/Contents";
 
 export default function Main() {
-  // const containerRef = useRef<HTMLDivElement>(null);
-  // useScrollAnimation(containerRef);
-
   return (
     <>
-      {/* <div
-        // ref={containerRef}
-        // className="w-full h-[100vh] fixed top-0 left-0"
-        className="fixed top-0 left-0 w-full overflow-y-auto"
-      >
-        <div className="h-[100vh]" ref={containerRef}></div>
-      </div> */}
       <Contents />
     </>
   );

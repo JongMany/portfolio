b2001a63e5faf31da4af53df0fcaeed1356c81f7
JongMany <blackberry1114@naver.com>
2024-07-09 16:51:21 +0900

    feature: 스크롤 애니메이션 구현

--- fe/src/pages/root/components/content/MyProfile.tsx
+++ fe/src/pages/root/components/content/MyProfile.tsx
@@ -1,5 +1,5 @@
 import { useInView } from "framer-motion";
-import React, { useRef } from "react";
+import { useRef } from "react";
 
 export default function MyProfile() {
   const ref = useRef<HTMLDivElement>(null);

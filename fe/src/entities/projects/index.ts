d2716f24cc6ebdd3e7ef35aeb056174e3e34e597
JongMany <blackberry1114@naver.com>
2024-07-29 14:23:49 +0900

    feat: 모바일 대응을 위해 코드 수정

--- fe/src/entities/projects/index.ts
+++ fe/src/entities/projects/index.ts
@@ -1,2 +1,3 @@
 export { default } from "./ui/project/Project";
+export { default as ProjectContainer } from "./ui/project/ProjectContainer";
 export { default as FileDownloadButton } from "./ui/download-button/FileDownloadButton";
--- fe/src/shared/libs/index.ts
+++ fe/src/shared/libs/index.ts
@@ -1,1 +1,2 @@
 export * from "./typing/useTypingAnimation";
+export * from "./media-query/useDeviceSize";

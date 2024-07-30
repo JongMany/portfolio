2e3b3aa10cfd9a171c09f45c124e75584c56922a
JongMany <blackberry1114@naver.com>
2024-07-30 10:22:39 +0900

    remove: FileDownloadButton 컴포넌트에서 훅으로 변경되기 전의 주석 코드 제거

--- fe/src/entities/projects/ui/download-button/FileDownloadButton.tsx
+++ fe/src/entities/projects/ui/download-button/FileDownloadButton.tsx
@@ -5,29 +5,6 @@ type Props = {
 };
 
 export default function FileDownload({ fileUrl }: Props) {
-  // const [isDownloading, setIsDownloading] = useState(false);
-  // const handleFileDownload = () => {
-  //   if (!isDownloading) {
-  //     setIsDownloading(true);
-  //     fetch(fileUrl)
-  //       .then((response) => response.blob())
-  //       .then((blob) => {
-  //         const url = window.URL.createObjectURL(new Blob([blob]));
-  //         const linkElem = document.createElement("a");
-  //         linkElem.href = url;
-  //         linkElem.download = fileUrl.split("/").pop() || "download";
-  //         document.body.appendChild(linkElem);
-  //         linkElem.click();
-
-  //         window.URL.revokeObjectURL(url);
-  //         setIsDownloading(false);
-  //       })
-  //       .catch((error) => {
-  //         console.error(`File download failed: ${error}`);
-  //         setIsDownloading(false);
-  //       });
-  //   }
-  // };
   const { isDownloading, handleFileDownload } = useDownloadFile(fileUrl);
 
   return (
